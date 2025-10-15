12f53922861afbead26651f53e1da47b47bd9531
Leandro <leandro.golub123@gmail.com>
2025-10-15 19:24:30 -0300

    feat: cria seção inicial

--- app/components/sections/home.vue
+++ app/components/sections/home.vue
@@ -1,14 +1,32 @@
 <script setup></script>
 
 <template>
-  <section id="home" class="bg-primary">
-    <h1 class="uppercase text-secondary">Leandro Golub</h1>
-    <h2 class="text-secondary/70">Desenvolvedor Front-end</h2>
-    <ul class="flex">
-      <li><i class="bi bi-envelope-fill text-secondary"></i></li>
-      <li><i class="bi bi-linkedin text-secondary"></i></li>
-      <li><i class="bi bi-github text-secondary"></i></li>
-    </ul>
+  <section id="home" class="bg-primary flex flex-col justify-center">
+    <div class="pl-24">
+      <h1
+        class="flex flex-col uppercase text-secondary text-9xl leading-24 mb-6 tracking-tight"
+      >
+        <span>Leandro</span> <span>Golub de Oliveira</span>
+      </h1>
+      <h2 class="text-secondary/70 text-2xl">Desenvolvedor Front-end</h2>
+      <ul class="flex gap-3 text-secondary text-2xl">
+        <li class="hover:text-accent cursor-pointer">
+          <a href="mailto:leandro.golub123@gmail.com" target="_blank"
+            ><i class="bi bi-envelope-fill"></i
+          ></a>
+        </li>
+        <li class="hover:text-accent cursor-pointer">
+          <a href="https://github.com/LeandroGOliv/" target="_blank"
+            ><i class="bi bi-github"></i
+          ></a>
+        </li>
+        <li class="hover:text-accent cursor-pointer">
+          <a href="https://www.linkedin.com/in/leandrogolivv/" target="_blank"
+            ><i class="bi bi-linkedin"></i
+          ></a>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
