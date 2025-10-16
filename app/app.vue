c823c6af221f4c58072e41610d1ce783a44f5a8c
Leandro <leandro.golub123@gmail.com>
2025-10-16 00:12:48 -0300

    feat: cria estrutura de arquivos necessaria

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@
   <LayoutAppHeader class="fixed top-0 z-50" />
   <SectionsHome class="min-h-screen w-full" />
   <SectionsAbout class="min-h-screen w-full" />
-  <!-- projetos/tecnologias(dividir em 2 componentes) -->
+  <SectionsProjectsAndStack class="min-h-screen w-full" />
   <!-- contato -->
   <!-- footer -->
 </template>
